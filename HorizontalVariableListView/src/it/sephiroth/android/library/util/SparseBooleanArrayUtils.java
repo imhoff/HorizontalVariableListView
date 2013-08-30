0888efd6439cb9d76dd9378f48e4bfcda105af04
Paul Imhoff <jan.paul.imhoff@googlemail.com>
2013-08-30 16:37:15 +1200

    Fixed error when saving the state of the array list on devices where SparseArrayList.clone() does not exist

--- HorizontalVariableListView/src/it/sephiroth/android/library/util/SparseBooleanArrayUtils.java
+++ HorizontalVariableListView/src/it/sephiroth/android/library/util/SparseBooleanArrayUtils.java
@@ -1,5 +1,23 @@
 package it.sephiroth.android.library.util;
 
 public class SparseBooleanArrayUtils {
+	/**
+	 * Threadsafe clone for backward compatibility as
+	 * {@link SparseBooleanArray#clone()} is supported first in 4.x APIs
+	 * 
+	 * @param orginal
+	 * @return a clone
+	 */
+	public static SparseBooleanArray clone(final SparseBooleanArray orginal) {
+		final SparseBooleanArray clone = new SparseBooleanArray();
 
+		synchronized (orginal) {
+			final int size = orginal.size();
+			for (int i = 0; i < size; i++) {
+				clone.put(i, orginal.get(i));
+			}
+		}
+
+		return clone;
+	}
 }
